424fbf3f6ef10d399f1689cbcee6867297ba5d65
Grzegorz <grzegosz007@gmail.com>
2024-04-09 18:21:58 +0200

    add homepage

--- Viso/ProjectApp/projectapp.client/src/App.jsx
+++ Viso/ProjectApp/projectapp.client/src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import LoginForm from './Components/LoginForm/LoginForm';
 import CreateUser from './Components/CreateUser/CreateUser';
+import HomePage from './Components/HomePage/HomePage';
 
 
 function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<LoginForm />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginForm />} />
                 <Route path="/createuser" element={<CreateUser />} />
             </Routes>
         </>
---
+++ Viso/ProjectApp/projectapp.client/src/Components/HomePage/Footer.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import styles from './HomePage.module.scss'
+
+
+function Footer() {
+    return (
+        <footer className={styles.footer_1}>
+            <nav>
+            Policy & Term
+                {/*<ul className={styles.footer_nav_links}>*/}
+                {/*    <li>1</li>*/}
+                {/*    <li>2</li>*/}
+                {/*    <li>3</li>*/}
+                {/*</ul>*/}
+            </nav>
+        </footer>
+    );
+}
+export default Footer;
