6f45c9e3c874a212484328e06e95abd3ab93dc15
Krzysztof Tarkowski <k.tarkowski@wp.pl>
2024-04-09 17:33:04 +0200

    Add Roles and Permitions to DB; Update model structure

---
+++ Viso/ProjectApp/ProjectApp.Server/Models/Permition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectApp.Server.Models;
+
+public partial class Permition
+{
+    public int PermitId { get; set; }
+
+    public string PermitName { get; set; } = null!;
+
+    public int RoleId { get; set; }
+}
