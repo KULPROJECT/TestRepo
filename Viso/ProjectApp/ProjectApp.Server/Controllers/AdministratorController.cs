a6d962d1cad36e289055732ca71b8b541aba1598
Krzysztof Tarkowski <k.tarkowski@wp.pl>
2024-05-22 14:24:05 +0200

    Add application comment entity to DB, add endpoints to show application status

--- Viso/ProjectApp/ProjectApp.Server/Controllers/AdministratorController.cs
+++ Viso/ProjectApp/ProjectApp.Server/Controllers/AdministratorController.cs
@@ -18,6 +18,14 @@ namespace ProjectApp.Server.Controllers
         }
 
         [HttpGet]
+        [Route("GetClients")]
+        public IActionResult GetClients()
+        {
+            List<Client> clientList = _dbContext.Clients.ToList();
+            return StatusCode(StatusCodes.Status200OK, clientList);
+        }
+
+        [HttpGet]
         [Route("GetClientsRequests")]
         public IActionResult GetClientsRequests()
         {
@@ -31,11 +39,10 @@ namespace ProjectApp.Server.Controllers
 
         [HttpPost]
         [Route("ChangeRequestStatus")]
-        public IActionResult ChangeRequestStatus(int[] data)
+        public IActionResult ChangeRequestStatus(string[] data)
         {
-            var clientID = data[0];
-            var newStatus = data[1];
-            if (int.IsEvenInteger(clientID)) return StatusCode(StatusCodes.Status409Conflict, "Wrong id format!");
+            var clientID = int.Parse(data[0]); 
+            var newStatus = int.Parse(data[1]);
 
             var client = _dbContext.Clients.FindAsync(clientID);
             if (client.IsFaulted)
@@ -50,6 +57,24 @@ namespace ProjectApp.Server.Controllers
                         "Client found but it has no client role assigned!");
                 clientRole.FirstOrDefault().RoleId = (int)ERoles.Restaurateur;
             }
+
+            if (newStatus == (int)ERestaurateurStatus.ToCorrect)
+            {
+                var comment = data[2];
+                if (!comment.IsNullOrEmpty())
+                {
+                    var applicationComment = new ApplicationComment()
+                    {
+                        ClientId = clientID,
+                        Comment = comment
+                    };
+                    _dbContext.ApplicationComments.Add(applicationComment);
+                } //should be prevented by frontend
+            }
+
+            {
+                
+            }
             _dbContext.SaveChangesAsync();
             return StatusCode(StatusCodes.Status200OK, "Client's application status changed successfully!");
         }
@@ -63,6 +88,8 @@ namespace ProjectApp.Server.Controllers
             if (client.IsFaulted)
                 return StatusCode(StatusCodes.Status406NotAcceptable, "No client of such id found!");
             client.Result.RestaurateurApplication = null;
+            var clientComment = _dbContext.ApplicationComments.Find(clientID);
+            if (clientComment!=null) _dbContext.ApplicationComments.Remove(clientComment);
             _dbContext.SaveChangesAsync();
             return StatusCode(StatusCodes.Status200OK, "Client's application resolved successfully!");
         }
--- Viso/ProjectApp/ProjectApp.Server/Controllers/ClientController.cs
+++ Viso/ProjectApp/ProjectApp.Server/Controllers/ClientController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ProjectApp.Server.Models;
 using ProjectApp.Server.Structures;
 
@@ -16,19 +18,12 @@ namespace ProjectApp.Server.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpGet]
-        [Route("GetClients")]
-        public IActionResult GetClients()
-        {
-            List<Client> clientList = _dbContext.Clients.ToList();
-            return StatusCode(StatusCodes.Status200OK, clientList);
-        }
 
         [HttpPost]
         [Route("ApplyForRestaurateur")]
         public IActionResult ApplyForRestaurateur(int clientID)
         {
-            if (clientID!=null)
+            if (clientID != null)
             {
                 var client = _dbContext.Clients.FindAsync(clientID);
                 if (client.Result == null)
@@ -38,10 +33,44 @@ namespace ProjectApp.Server.Controllers
                 //    return StatusCode(StatusCodes.Status403Forbidden,
                 //        "Request is being verified, please wait for a message"); TODO: MOVE TO FRONTEND
 
-                client.Result.RestaurateurApplication = (int) ERestaurateurStatus.Applied;
+                client.Result.RestaurateurApplication = (int)ERestaurateurStatus.Applied;
                 _dbContext.SaveChangesAsync();
                 return StatusCode(StatusCodes.Status200OK, "Application saved to database");
             }
+
+            return StatusCode(StatusCodes.Status409Conflict, "Wrong id format!");
+        }
+
+        [HttpPost]
+        [Route("GetClientApplicationStatus")]
+        public IActionResult GetClientApplicationStatus(int clientID)
+        {
+            if (clientID != null)
+            {
+                var client = _dbContext.Clients.FindAsync(clientID);
+                if (client.Result == null)
+                    return StatusCode(StatusCodes.Status406NotAcceptable, "No such client!");
+
+                var restaurateurApplication = client.Result.RestaurateurApplication;
+                if (restaurateurApplication == null)
+                    return StatusCode(StatusCodes.Status406NotAcceptable, "Client did not apply to become a restaurateur");
+
+                var applicationStatus = (ERestaurateurStatus) restaurateurApplication;
+                string[] response = [applicationStatus.ToString(), null];
+                if (client.Result.RestaurateurApplication == (int)ERestaurateurStatus.ToCorrect)
+                {
+                    var applicationComment =
+                        _dbContext.ApplicationComments.Find(clientID);
+                    if (applicationComment!=null)
+                    {
+                        response[1] = applicationComment.Comment;
+                    }
+                }
+
+                _dbContext.SaveChangesAsync();
+                return StatusCode(StatusCodes.Status200OK, response);
+            }
+
             return StatusCode(StatusCodes.Status409Conflict, "Wrong id format!");
         }
     }
